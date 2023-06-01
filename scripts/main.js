779610ada5247202bea1212064667d3cef5fc41a
W_K.O <45578619+WKO8@users.noreply.github.com>
2023-05-31 23:12:24 -0300

    Added CPF input and value in words

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ function generateLandscapePDF() {
 
     var clientName = document.getElementById('clientName');
     var clientLastname = document.getElementById('clientLastname');
+    var clientCPF = document.getElementById('clientCPF');
     var clientDate = document.getElementById('clientDate');
     var clientDescription = document.getElementById('clientDescription');
     var clientServiceValue = document.getElementById('clientServiceValue');
@@ -58,8 +59,8 @@ function generateLandscapePDF() {
     doc.setFontSize(20);
     doc.setFontType("normal");
     doc.text(25, 90, "Recebemos de: " + clientName.value +  " " + clientLastname.value);
-    doc.text(25, 100, "CPF/CNPJ: " + clientIdentification);
-    doc.text(25, 110, "A importância de: R$" + clientServiceValue.value);
+    doc.text(25, 100, "CPF/CNPJ: " + clientCPF.value);
+    doc.text(25, 110, "A importância de: R$" + clientServiceValue.value + " (" + clientServiceValue.value.extenso().toUpperCase() + " REAIS)");
     doc.text(25, 120, "Referente à: " + clientDescription.value);
     var year = clientDate.value.slice(0,4);
     var month = clientDate.value.slice(5,7);
@@ -150,6 +151,35 @@ function generatePortraitPDF() {
 
     doc.save('a4.pdf')
 }
+
+// Prototype function to transform int to string
+String.prototype.extenso = function(c){
+    var ex = [
+        ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"],
+        ["dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"],
+        ["cem", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"],
+        ["mil", "milhão", "bilhão", "trilhão", "quadrilhão", "quintilhão", "sextilhão", "setilhão", "octilhão", "nonilhão", "decilhão", "undecilhão", "dodecilhão", "tredecilhão", "quatrodecilhão", "quindecilhão", "sedecilhão", "septendecilhão", "octencilhão", "nonencilhão"]
+    ];
+    var a, n, v, i, n = this.replace(c ? /[^,\d]/g : /\D/g, "").split(","), e = " e ", $ = "real", d = "centavo", sl;
+    for(var f = n.length - 1, l, j = -1, r = [], s = [], t = ""; ++j <= f; s = []){
+        j && (n[j] = (("." + n[j]) * 1).toFixed(2).slice(2));
+        if(!(a = (v = n[j]).slice((l = v.length) % 3).match(/\d{3}/g), v = l % 3 ? [v.slice(0, l % 3)] : [], v = a ? v.concat(a) : v).length) continue;
+        for(a = -1, l = v.length; ++a < l; t = ""){
+            if(!(i = v[a] * 1)) continue;
+            i % 100 < 20 && (t += ex[0][i % 100]) ||
+            i % 100 + 1 && (t += ex[1][(i % 100 / 10 >> 0) - 1] + (i % 10 ? e + ex[0][i % 10] : ""));
+            s.push((i < 100 ? t : !(i % 100) ? ex[2][i == 100 ? 0 : i / 100 >> 0] : (ex[2][i / 100 >> 0] + e + t)) +
+            ((t = l - a - 2) > -1 ? " " + (i > 1 && t > 0 ? ex[3][t].replace("ão", "ões") : ex[3][t]) : ""));
+        }
+        a = ((sl = s.length) > 1 ? (a = s.pop(), s.join(" ") + e + a) : s.join("") || ((!j && (n[j + 1] * 1 > 0) || r.length) ? "" : ex[0][0]));
+        a && r.push(a + (c ? (" " + (v.join("") * 1 > 1 ? j ? d + "s" : (/0{6,}$/.test(n[0]) ? "de " : "") + $.replace("l", "is") : j ? d : $)) : ""));
+    }
+    return r.join(e);
+}
+
+
+console.log('153'.extenso().toUpperCase());
+
 
 var form = document.getElementById('clientForm');
 
